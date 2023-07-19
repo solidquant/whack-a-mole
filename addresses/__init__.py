1053edae78865c901ddc90eb61da3f4ead8a8fec
Solid Quant <solidquant@gmail.com>
2023-07-19 12:50:27 +0900

    Example strategy #1: DEX arb. base

--- addresses/__init__.py
+++ addresses/__init__.py
@@ -1,10 +1,4 @@
-from addresses.arbitrum import TOKENS as ARBITRUM_TOKENS
-from addresses.arbitrum import POOLS as ARBITRUM_POOLS
-
 from addresses.ethereum import TOKENS as ETHEREUM_TOKENS
 from addresses.ethereum import POOLS as ETHEREUM_POOLS
 from addresses.ethereum import SIMULATION_HANDLERS as ETHEREUM_SIMULATION_HANDLERS
 from addresses.ethereum import EXECUTION_HANDLERS as ETHEREUM_EXECUTION_HANDLERS
-
-from addresses.polygon import TOKENS as POLYGON_TOKENS
-from addresses.polygon import POOLS as POLYGON_POOLS
--- configs.py
+++ configs.py
@@ -3,13 +3,7 @@ from dotenv import load_dotenv
 
 from addresses import (
     ETHEREUM_TOKENS,
-    POLYGON_TOKENS,
-    ARBITRUM_TOKENS,
-
     ETHEREUM_POOLS,
-    POLYGON_POOLS,
-    ARBITRUM_POOLS,
-
     ETHEREUM_SIMULATION_HANDLERS,
     ETHEREUM_EXECUTION_HANDLERS,
 )
@@ -18,23 +12,17 @@ load_dotenv(override=True)
 
 RPC_ENDPOINTS = {
     'ethereum': os.getenv('ETHEREUM_HTTP_RPC_URL'),
-    'polygon': os.getenv('POLYGON_HTTP_RPC_URL'),
-    'arbitrum': os.getenv('ARBITRUM_HTTP_RPC_URL'),
 }
 
 WS_ENDPOINTS = {
     'ethereum': os.getenv('ETHEREUM_WS_RPC_URL'),
-    'polygon': os.getenv('POLYGON_WS_RPC_URL'),
-    'arbitrum': os.getenv('ARBITRUM_WS_RPC_URL'),
 }
 
 TOKENS = {
     'ethereum': ETHEREUM_TOKENS,
-    'polygon': POLYGON_TOKENS,
-    'arbitrum': ARBITRUM_TOKENS,
 }
 
-POOLS = ETHEREUM_POOLS + POLYGON_POOLS + ARBITRUM_POOLS
+POOLS = ETHEREUM_POOLS
 
 SIMULATION_HANDLERS = {
     'ethereum': ETHEREUM_SIMULATION_HANDLERS,
